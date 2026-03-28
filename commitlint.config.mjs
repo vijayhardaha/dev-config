e17e5dffd39eb9035f18c2f541f5a287d7ec4804
Vijay Hardaha <vijayhardaha@proton.me>
2026-03-28 08:12:48 +0530

    fix: add rules key in src/commitlint/index.js

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -9,15 +9,6 @@
  * =====================================================================
  */
 
-/** @type {import('@commitlint/types').UserConfig} */
-const config = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 50],
-    'body-max-line-length': [2, 'always', 72],
-    'body-case': [2, 'always', ['sentence-case', 'lower-case']],
-    'subject-case': [0],
-  },
-};
+import commitlintConfig from './src/commitlint/index.js';
 
-export default config;
+export default commitlintConfig;
